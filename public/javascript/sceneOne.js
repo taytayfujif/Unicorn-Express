919cdf1526073fc656e9d8e622b6c509c758ef61
Alex <eieghtbitdolphin@gmail.com>
2019-04-16 22:47:55 -1000

    fin to add risky code throughout the night

--- public/javascript/sceneOne.js
+++ public/javascript/sceneOne.js
@@ -6,12 +6,12 @@ class sceneOne extends Phaser.Scene {
 
     preload() {
         console.log(this)
-        var choiceOneRect = this.add.graphics();
+
 
         // choiceOneText.(0, 100, 800, 100);
 
         this.load.image('background','../assets/GameOne/Background.png');
-        this.load.image('topBox','../assets/GameOne/Opening_Box.png');
+        this.load.image('topBox','../assets/GameOne/Opening_Box.png'); 
         this.load.image('bottomBox','../assets/GameOne/Bottom_Box.png');
         this.load.image('card1','../assets/GameOne/Cards/One/Pride.png'); //card one
         this.load.image('card2','../assets/GameOne/Cards/One/Tax.png'); //card two
@@ -25,32 +25,26 @@ class sceneOne extends Phaser.Scene {
 
     create() { //loads in the images and stuff for game
 
-        this.score = 0;
+        this.score = 0; //the score that is kept when the user does something
+        this.scenerio = 0; // the scenerios in the scene
+        this.background = this.add.image(400,300,'background'); //makes the background of the game
 
-        this.time.addEvent({ 
-            delay: 1000, 
-            callback: () => {
-                this.timerText.text -= 1
-            }, 
-            callbackScope: this,
-            repeat: 9
-        })
-
-        this.sendData = function(obtainedScore) {
-            dataBase.collection('users').doc(auth.currentUser.uid).update({
-                score: obtainedScore
-            }).then(() => {
-                console.log("low key it worked! Check the score");
-            })
+        this.cards = {
+            questionOne: {
+                question: "What are the Benefits of owning a Home?",
+                choices: [
+                    {card: this.add.sprite(225,176,'card1'),correct: false}, //card one
+                    {card: this.add.sprite(575,176,'card2'), correct: false}, //card two
+                    {card: this.add.sprite(225,424,'card3'), correct: false}, //card thre
+                    {card: this.add.sprite(575,424,'card4'), correct: true} //card four 
+                ]
+            }
         }
 
-        this.background = this.add.image(400,300,'background');
-        this.cardOne = this.add.sprite(225,176,'card1'); //card one
-        this.cardTwo = this.add.sprite(575,176,'card2'); //card two
-        this.cardTwo = this.add.sprite(225,424,'card3'); //card two
-        this.cardTwo = this.add.sprite(575,424,'card4'); //card two
 
-        this.ScenerioText = this.make.text({
+        //loads all the cards in the game 
+
+        this.ScenerioText = this.make.text({ //One of the Scenario text for the 
             x:105,
             y:10,
             boundsAlignH: "center", 
@@ -62,7 +56,9 @@ class sceneOne extends Phaser.Scene {
             }
         });
 
-        this.timerText = this.make.text({
+
+        /* timer stuff */
+        this.timerText = this.make.text({ //timer text 
             x: 10,
             y:10,
             boundsAlignH: "center", 
@@ -74,66 +70,32 @@ class sceneOne extends Phaser.Scene {
             }
         });
 
-        //controls
+        this.questionOne = this.add.group([this.cardOne,this.cardTwo,this.cardThree,this.cardFour],"questionOne");
+        console.log(this.questionOne);
 
-        this.cardOne.setInteractive().on('pointerhover', () => 
-        { 
-            
-        }).on('pointerdown', () => {
-            console.log('clicked Option One');
-            this.ScenerioText.destroy();
-            this.topBox.destroy();
-            this.cardOne.destroy();
-            this.cardTwo.destroy();
-            this.bottomBox.destroy();
-            this.score += 500;
-
-            this.sendData(this.score);
-
-            this.ScenerioText = this.make.text({
-                x:800/4,
-                y:600/2,
-                boundsAlignH: "center", 
-                boundsAlignV: "middle",
-                text: 'Thanks for submitting! You Get 500 Points for that',
-                style: {
-                    font: '20px monospace',
-                    fill: '#000000'
-                }
-            });
+        this.time.addEvent({  //adds the time event 
+            delay: 1000, 
+            callback: () => {
+                this.timerText.text -= 1
+            }, 
+            callbackScope: this,
+            repeat: 9
         });
+        /* timer stuff end */
 
-        this.cardTwo.setInteractive().on('pointerhover', () => 
-        { 
-            
-        }).on('pointerdown', () => {
-            console.log('clicked Option Two');
-            this.ScenerioText.destroy();
-            this.topBox.destroy();
-            this.cardOne.destroy();
-            this.cardTwo.destroy();
-            this.bottomBox.destroy();
-            this.score -= 500;
+        /* Methods */
 
-            this.sendData(this.score);
-
-            this.ScenerioText = this.make.text({
-                x:800/4,
-                y:600/2,
-                boundsAlignH: "center", 
-                boundsAlignV: "middle",
-                text: 'Thanks for submitting! You Get 500 Points for that',
-                style: {
-                    font: '20px monospace',
-                    fill: '#000000'
-                }
-            });
-        });
+        this.sendData = function(obtainedScore) { //sends and updates thee score when the game is over
+            dataBase.collection('users').doc(auth.currentUser.uid).update({
+                score: obtainedScore
+            }).then(() => {
+                console.log("low key it worked! Check the score");
+            })
+        }
     }
 
     update(delta) { //game loop
-        // if(this.key_A.isDown)
-        //     this.image.x-=4;
+
     }
 
     render() {
