44ce21e6f58c725aeae14d3d4b2b07a48bba7d76
Alex <eieghtbitdolphin@gmail.com>
2019-04-19 14:15:56 -1000

    Added riot to the project

---
+++ public/components/footer.js
@@ -0,0 +1,155 @@
+<Main-Footer>
+
+    <div class="page-footer">
+
+            <div class="container footerContainer"> <!--Main Container it is in-->
+                
+                <div class="row footerRow verticalDivider"> <!--the general row it is in-->
+
+                    <div class="col s12 m3 hide-on-small-only column"> <!--Column 1~Image-->
+
+                        <img src="../assets/Logo_footer.png" alt="A flat illistrated rndom">
+
+                    </div>
+
+                    <div class="col s12 m3 column"> <!--Column 2~Links-->
+
+                        <div class="row footerRow"> <!--Level 1 of a inner Row-->
+
+                            <div class="col s12 grey-text text-lighten-1 light">
+                                <h6 class="footerSubHeader">
+                                    Links
+                                </h6>
+                            </div>
+
+                            <div class="col s12">
+
+                                <div class="row footerRow"> <!--Level 2 of a inner Row-->
+
+                                    <a href="../html/newArticle.html" class="col s12 grey-text text-lighten-1 light footerContent">Corner Club</a>
+                                    <a href="../html/article.html" class="col s12 grey-text text-lighten-1 light footerContent">Games</a>
+                                    <a href="../html/newArticle2.html" class="col s12 grey-text text-lighten-1 light footerContent">Home Ownership</a>
+                                    <a href="../html/newArticle3.html" class="col s12 grey-text text-lighten-1 light footerContent">Present Saving</a>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col s12 m3 column"> <!--Column 2~Links-->
+
+                        <div class="row footerRow"> <!--Level 1 of a inner Row-->
+
+                            <div class="col s12 grey-text text-lighten-1 light">
+                                <h6 class="footerSubHeader">
+                                    Contact
+                                </h6>
+                            </div>
+
+                            <div class="col s12">
+
+                                <div class="row footerRow"> <!--Level 2 of a inner Row-->
+
+                                    <div href="../html/article.html" class="col s12 grey-text text-lighten-1 light footerContent">
+                                        <i class="material-icons left">phone</i>
+                                        <span>(808)420-6969</span>
+                                    </div>
+
+                                    <div href="../html/article.html" class="col s12 grey-text text-lighten-1 light footerContent">
+                                        <i class="material-icons left">mail</i>
+                                        <span>placeholder@gmail.com</span>
+                                    </div>
+
+                                    <div href="../html/article.html" class="col s12 grey-text text-lighten-1 light footerContent">
+                                        <i class="material-icons left">business</i>
+                                        <span>1539 N. Calvert</span>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col s12 m3 column center-align valign-wrapper"> <!--Column 3~Copyright-->
+                        <p class="grey-text lighten-1 bold copyright">© Copyright 2019 - The Unicorn Express - All Rights Reserved</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        
+        <style>
+            :scope
+
+            .page-footer {
+                padding-top: 5px;
+                background: #F3F3F3;
+                box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+            }
+
+            .page-footer.dropdown > * {
+                margin-bottom: 0;
+            }
+            .footerRow {
+                margin-bottom: 0;
+            }
+
+            .footerContainer {
+                width: 100%;
+            }
+
+            .footerSubHeader {
+                margin: 0.3rem 0 0.46rem 0;
+            }
+
+            .footerSubHeader::after {
+                content: ""; 
+                display: block; 
+                width: 10vh; 
+                padding-top: 5px; 
+                border-bottom: 0.25px solid #77FFC6;
+            }
+
+            .footerContent {
+                font-size: 13px;
+            }
+
+            .verticalDivider {
+                position: relative;
+            }
+
+            .verticalDivider:after {
+                clear: both;
+                content: " ";
+                display: block;
+                height: 0;
+                visibility: hidden;
+            }
+
+            .verticalDivider .column:not(:first-child):after, .vertical-divider .columns:not(:first-child):after{
+                background:#c3c3c3;
+                bottom: 0;
+                height: 50%;
+                content: " ";
+                margin-left: -5%;
+                position: absolute;
+                top: 25%;
+                width: 1px;
+            }
+
+            .copyright {
+                font-size: 10px;
+                font-weight: bold;
+            }
+
+        </style>
+
+</Main-Footer>
+
---
+++ public/components/footerArticle.js
@@ -0,0 +1,55 @@
+<Footer-Sub>
+
+<nav class=" nav-wrapper row z-depth-0 homeHeader">
+
+<div class="row">
+    
+    <div class="col s12 m1 left">
+        <a href="newIndex.html">
+            <div class="brand-logo">
+             <img src="../assets/Logo_footer.png" alt="Neado" />
+            </div>
+        </a>
+    </div>
+
+    <div class="col m11 right">
+
+        <div class="row contentRow">
+
+
+            <div class="col m3 center-align headerContent">
+                <a href="newArticle.html">
+                    <h4>Corner Club</h4>
+                </a>
+                
+            </div>
+
+            <div class="col m3 center-align headerContent">
+                <a href="newArticle3.html">
+                    <h4>Ownership</h4>
+                </a>
+                
+            </div>
+
+            <div class="col m3 center-align headerContent">
+                <a href="newArticle4.html">
+                    <h4>Why Hawaii</h4>
+                </a>
+                
+            </div>
+
+            <div class="col m3 center-align headerContent hide-on-small-only">
+                <a href="game.html">
+                    <h4>Games</h4>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+</nav>
+
+</Footer-Sub>
