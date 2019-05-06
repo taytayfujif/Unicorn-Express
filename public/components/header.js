5aa9eab84dba87174f71bf8acced4986cc205783
Alex <eieghtbitdolphin@gmail.com>
2019-05-05 20:36:54 -1000

    added some libaries'

---
+++ public/components/header.js
@@ -0,0 +1,82 @@
+<Main-Header>
+ <nav class=" nav-wrapper row z-depth-0 homeHeader hide-on-med-and-down">
+            <div class="col m5 header-Left">
+
+                <div class="row">
+
+                    <div class="col m5">
+                        <a href="newArticle.html">
+                        <h5>Corner Club</h5>
+                        </a>
+                    </div>
+
+                    <div class="col m5 offset-m2">
+                        <a href="newArticle2.html">
+                        <h5>Present Saving</h5>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col m2">
+                <img src="../assets/Logo.png" alt="The Logo of the Website" id="headerLogo">
+            </div>
+
+            <div class="col m5 header-Right">
+
+                <div class="row">
+
+                    <div class="col m5">
+                        <a href="newArticle3.html">
+                            <h5>Obtaining a House</h5>
+                        </a>
+                    </div>
+        
+                    <div class="col m5 offset-m2">
+                        <a href="newArticle4.html">
+                            <h5 class="floatRight">Why Here?</h5>
+                        </a>
+                    </div>
+        
+                </div>
+
+            </div>
+    </nav>
+
+    <style>
+        :scope
+        #headerLogo {
+        display: table;
+        margin: auto;
+    }
+    h5 {
+        margin: 0;
+    }
+    .homeHeader {
+        height: auto;
+        background-color: rgba(0,0,0,0);
+    }
+    .header-Left, .header-Right {
+        margin-top: 30vh;
+    }
+
+    .floatRight {
+        text-align: right;
+    }
+
+    h5 {
+        color: #FF8AB4;
+        padding: 0.4vh;
+        border-bottom: 1px solid #77FFC6;
+    }
+
+    h5 {
+        transition: 1s color ease;
+    }
+    a:hover h5 {
+        color: #B3EDFF;
+    }
+    </style>
+</Main-Header>
---
+++ public/components/headerArticle.js
@@ -0,0 +1,81 @@
+<Footer-Sub>
+
+<nav class=" nav-wrapper row z-depth-0 homeHeader">
+
+<div class="row">
+    
+    <div class="col s12 m1 left">
+        <a href="newIndex.html">
+            <div class="brand-logo">
+             <img src="../assets/Logo_footer.png" alt="Neado" />
+            </div>
+        </a>
+    </div>
+
+    <div class="col m11 right hide-on-small-only">
+
+        <div class="row contentRow">
+
+
+            <div class="col m3 center-align headerContent">
+                <a href="newArticle.html">
+                    <h4>Corner Club</h4>
+                </a>
+                
+            </div>
+
+            <div class="col m3 center-align headerContent">
+                <a href="newArticle3.html">
+                    <h4>Ownership</h4>
+                </a>
+                
+            </div>
+
+            <div class="col m3 center-align headerContent">
+                <a href="newArticle4.html">
+                    <h4>Why Hawaii</h4>
+                </a>
+                
+            </div>
+
+            <div class="col m3 center-align headerContent hide-on-small-only">
+                <a href="game.html">
+                    <h4>Games</h4>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+</nav>
+
+<style>
+    :scope
+    .brand-logo > img {
+        height: 97.41px;
+    }
+    .homeHeader {
+        height: 20%;
+        background-color: rgba(0,0,0,0);
+    }
+    .contentRow {
+        margin: 0;
+        height: 100%;
+    }
+    .headerContent > a, .col > h4{
+        color: #FF8AB4;
+    }
+    .headerContent > a > h4{
+        font-weight: lighter;
+        transition: all 1s ease;
+    }
+
+    .headerContent > a > h4:hover {
+        color: #B3EDFF;
+    }
+</style>
+
+</Footer-Sub>
